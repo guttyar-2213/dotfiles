e53addd98de7a55a4a546918b4996545e554e092
guttyar-2213 <guttyar2213.00@gmail.com>
2022-12-29 17:45:07 +0900

    なぜかapplyに失敗するので一時的に削除

--- run_once_install-packages.sh
+++
@@ -1,9 +0,0 @@
-{{ if eq .chezmoi.os "linux" -}}
-#!/bin/bash
-sudo apt install vim
-{{ else if eq .chezmoi.os "darwin" -}}
-#!/bin/bash
-brew install vim
-{{ end -}}
-
-curl -fLo ~/.vim/pack/jetpack/opt/vim-jetpack/plugin/jetpack.vim --create-dirs https://raw.githubusercontent.com/tani/vim-jetpack/master/plugin/jetpack.vim
